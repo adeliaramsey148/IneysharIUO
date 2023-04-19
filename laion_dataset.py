f5e95e634a83561ce70b67b31d404b5c6a719e91
adeliaramsey148 <104143460+adeliaramsey148@users.noreply.github.com>
2023-04-19 21:22:04 +0300

    Update laion_dataset.py

--- laion_dataset.py
+++ laion_dataset.py
@@ -10,7 +10,6 @@ from minigpt4.datasets.datasets.base_dataset import BaseDataset
 
 
 class LaionDataset(BaseDataset):
-    def __init__(self, vis_processor, text_processor, location):
         super().__init__(vis_processor=vis_processor, text_processor=text_processor)
 
         self.inner_dataset = wds.DataPipeline(
@@ -18,7 +17,6 @@ class LaionDataset(BaseDataset):
             wds.tarfile_to_samples(handler=wds.warn_and_continue),
             wds.shuffle(1000, handler=wds.warn_and_continue),
             wds.decode("pilrgb", handler=wds.warn_and_continue),
-            wds.to_tuple("jpg", "json", handler=wds.warn_and_continue),
             wds.map_tuple(self.vis_processor, handler=wds.warn_and_continue),
             wds.map(self.to_dict, handler=wds.warn_and_continue),
         )
